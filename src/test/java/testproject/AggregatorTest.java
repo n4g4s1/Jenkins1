0a18b580f16c9bf39759726d8a9128fd32664f9a
Neejiii <hohlysheva_o@mail.ru>
2021-12-09 19:31:22 +0300

    test

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,12 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+    @Test
+    public void testReset(){
+ 	Aggregator agr = new Agregator();
+	agr.addValue(1D);
+	agr.reset();
+	assertThat(agr.getSum()).isEqualTo(0);
+	}
 }
+
